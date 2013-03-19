1970fed4c9fa99d35706f9594cd47e8ca1644a38
Mike Martin <mike.martin@rackspace.com>
2013-03-19 13:10:18 -0500

    Creates initial skeleton for challenge2

---
+++ challenge2.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+# Copyright 2013 Mike Martin <mike.martin@rackspace.com>
+# Challenge 2: Write a script that clones a server (takes an image and deploys the image as a new server).
+# Worth 2 Points
+
+require "inifile"
+require "openstack/compute"
