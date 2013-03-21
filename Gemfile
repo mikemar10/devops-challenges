84736c87e1901cab2fa088426e5d83ff08fb7b92
Mike Martin <mike.martin@rackspace.com>
2013-03-21 14:57:53 -0500

    Switches challenge2 to fog

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source "https://rubygems.org"
+gem "inifile"
+gem "fog", :git => "https://github.com/fog/fog.git"
--- challenge2.rb
+++ challenge2.rb
@@ -4,5 +4,8 @@
 # Challenge 2: Write a script that clones a server (takes an image and deploys the image as a new server).
 # Worth 2 Points
 
+require "bundler/setup"
 require "inifile"
-require "openstack/compute"
+require "fog"
+
+
